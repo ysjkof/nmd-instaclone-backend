81d7318c3a8d98f6bbb9d72612db6d9857a72540
seongjin <ysjkof@gmail.com>
2021-04-29 21:24:16 +0900

    Protecting Resolvers

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,40 +1,42 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+const resolverFn = async (_, { firstName, lastName, username, email, password: newPassword }, { loggedInUser }) => {
+  // const verifiedToken = await jwt.verify(token, process.env.SECRET_KEY); 아래는 ES6문법으로 verifiedToken 안에 id를 풀었다. updateUser의 where에 쓰려고
+  // const { id } = await jwt.verify(token, process.env.SECRET_KEY);
+  console.log(loggedInUser);
+  let uglyPassword = null;
+  if (newPassword) {
+    uglyPassword = await bcrypt.hash(newPassword, 10);
+  }
+  const updatedUser = await client.user.update({
+    where: {
+      id: loggedInUser.id,
+    },
+    data: {
+      firstName,
+      lastName,
+      username,
+      email,
+      // uglyPassword가 true면 password:uglyPassword가 된다.
+      ...(uglyPassword && { password: uglyPassword }),
+    },
+  });
+  if (updatedUser.id) {
+    return {
+      ok: true,
+    };
+  } else {
+    return {
+      ok: false,
+      error: "Could not update profile.",
+    };
+  }
+};
 
 export default {
   Mutation: {
-    editProfile: async (_, { firstName, lastName, username, email, password: newPassword }, { loggedInUser }) => {
-      // const verifiedToken = await jwt.verify(token, process.env.SECRET_KEY); 아래는 ES6문법으로 verifiedToken 안에 id를 풀었다. updateUser의 where에 쓰려고
-      // const { id } = await jwt.verify(token, process.env.SECRET_KEY);
-      console.log(loggedInUser);
-      let uglyPassword = null;
-      if (newPassword) {
-        uglyPassword = await bcrypt.hash(newPassword, 10);
-      }
-      const updatedUser = await client.user.update({
-        where: {
-          id: loggedInUser.id,
-        },
-        data: {
-          firstName,
-          lastName,
-          username,
-          email,
-          // uglyPassword가 true면 password:uglyPassword가 된다.
-          ...(uglyPassword && { password: uglyPassword }),
-        },
-      });
-      if (updatedUser.id) {
-        return {
-          ok: true,
-        };
-      } else {
-        return {
-          ok: false,
-          error: "Could not update profile.",
-        };
-      }
-    },
+    editProfile: protectedResolver(resolverFn),
   },
 };
--- users/users.utils.js
+++ users/users.utils.js
@@ -17,3 +17,24 @@ export const getUser = async (token) => {
     return null;
   }
 };
+
+// export function protectedResolver(ourResolver) {
+//   return function (root, args, context, info) {
+//     if (!context.loggedInUser) {
+//       return {
+//         ok: false,
+//         error: "Please log in to perform this action.",
+//       };
+//     }
+//     return ourResolver(root, args, context, info);
+//   };
+// }
+export const protectedResolver = (ourResolver) => (root, args, context, info) => {
+  if (!context.loggedInUser) {
+    return {
+      ok: false,
+      error: "Please log in to perform this action.",
+    };
+  }
+  return ourResolver(root, args, context, info);
+};
