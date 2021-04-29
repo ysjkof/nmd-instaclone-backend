92c010f2fab4027eba6c1b10868c7927f2ec3f6e
seongjin <ysjkof@gmail.com>
2021-04-29 20:27:01 +0900

    send token goodway

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -4,16 +4,17 @@ import client from "../../client";
 
 export default {
   Mutation: {
-    editProfile: async (_, { firstName, lastName, username, email, password: newPassword, token }) => {
+    editProfile: async (_, { firstName, lastName, username, email, password: newPassword }, { loggedInUser }) => {
       // const verifiedToken = await jwt.verify(token, process.env.SECRET_KEY); 아래는 ES6문법으로 verifiedToken 안에 id를 풀었다. updateUser의 where에 쓰려고
-      const { id } = await jwt.verify(token, process.env.SECRET_KEY);
+      // const { id } = await jwt.verify(token, process.env.SECRET_KEY);
+      console.log(loggedInUser);
       let uglyPassword = null;
       if (newPassword) {
         uglyPassword = await bcrypt.hash(newPassword, 10);
       }
       const updatedUser = await client.user.update({
         where: {
-          id,
+          id: loggedInUser.id,
         },
         data: {
           firstName,
