332902799a3348908c9ed8e6302a5ccbd2319feb
seongjin <ysjkof@gmail.com>
2021-04-30 09:21:54 +0900

    file upload express local server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
 
 const PORT = process.env.PORT;
-const server = new ApolloServer({
+const apollo = new ApolloServer({
   resolvers,
   typeDefs,
   context: async ({ req }) => {
@@ -18,7 +18,8 @@ const server = new ApolloServer({
 
 const app = express();
 app.use(logger("tiny"));
-server.applyMiddleware({ app });
+app.use("/static", express.static("uploads"));
+apollo.applyMiddleware({ app });
 app.listen({ port: PORT }, () => {
   console.log(`✅ Server is running on http://localhost:${PORT}/`);
 });
