2875d5f892cffbf62f84ca35c18a79d500a20fab
seongjin <ysjkof@gmail.com>
2021-05-16 20:12:07 +0900

    DM finish

--- server.js
+++ server.js
@@ -12,12 +12,34 @@ const apollo = new ApolloServer({
   resolvers,
   typeDefs,
   uploads: false,
-  context: async ({ req }) => {
-    if (req) {
+  context: async (ctx) => {
+    if (ctx.req) {
       return {
-        loggedInUser: await getUser(req.headers.token),
+        loggedInUser: await getUser(ctx.req.headers.token),
+      };
+    } else {
+      const {
+        connection: { context },
+      } = ctx;
+      return {
+        loggedInUser: context.loggedInUser,
       };
     }
+  },
+  subscriptions: {
+    // onConnect는 connection되는 순간 context에 HTTP header를 준다.  === 아래 리턴한 loggedInUser는 위에 ctx.connection.context.loggedInUser로 간다.
+    // 웹소켓으로 연결할 때 딱 한 번만 불려진다.
+    // http request일때는 상관없지만 subscription이 웹소켓을 쓰는데 웹소켓에는 request가 없다.
+    // 이렇게 context에 전달하면 subscribe(웹소켓)에서 context를 쓸 수 있다.
+    onConnect: async ({ token }) => {
+      if (!token) {
+        throw new Error("You can't listen.");
+      }
+      const loggedInUser = await getUser(token);
+      return {
+        loggedInUser,
+      };
+    },
   },
 });
 
